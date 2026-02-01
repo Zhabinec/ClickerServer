758258526a07716a8cfe38420731141c5ba90408
Matvey <sinema3d00@mail.ru>
2026-02-01 23:11:40 +0300

    Задаю игрока дальше и изучаю параметры

--- Assets/ClickerGame/Scripts/CoreGame/Network/ClickerPlayer.cs
+++ Assets/ClickerGame/Scripts/CoreGame/Network/ClickerPlayer.cs
@@ -1,3 +1,4 @@
+using Game.Clicker.Domain;
 using Mirror;
 using UnityEngine;
 
@@ -15,12 +16,90 @@ namespace Game.Clicker.Network
 
         private double _nextTapTime;
 
+        public static event System.Action<ClickerState> OnLocalStateChanged;
+        public static event System.Action<string> OnLocalError;
+
         public override void OnStartServer()
         {
             _balance = 0;
             _multiplier = 1;
             _upgradeLevel = 0;
             _nextTapTime = 0.0;
+        }
+
+        public override void OnStartLocalPlayer()
+        {
+            CmdRequestState();
+        }
+
+        //Вызываются Client API
+        public void Tap()
+        {
+            if (!isLocalPlayer) return;
+            CmdTap();
+        }
+
+        public void BuyUpgrade()
+        {
+            if (!isLocalPlayer) return;
+            CmdBuyUpgrade();
+        }
+
+        // Command - это клиент -> сервер
+        [Command]
+        private void CmdRequestState()
+        {
+            TargetStateChanged(connectionToClient, MakeState());
+        }
+
+        [Command]
+        private void CmdTap()
+        {
+            if (NetworkTime.time < _nextTapTime) return;
+            _nextTapTime = NetworkTime.time + _tapCooldownSeconds;
+
+            _balance += _multiplier;
+            TargetStateChanged(connectionToClient, MakeState());
+        }
+
+        [Command]
+        private void CmdBuyUpgrade()
+        {
+            int price = GetUpgradePrice(_upgradeLevel);
+            if (_balance < price)
+            {
+                TargetError(connectionToClient, "not_enough_currency");
+                return;
+            }
+
+            _balance -= price;
+            _upgradeLevel += 1;
+            _multiplier += 1;
+
+            TargetStateChanged(connectionToClient, MakeState());
+        }
+
+        // Это наоборот Server -> Client (только этому игроку)
+        [TargetRpc]
+        private void TargetStateChanged(NetworkConnectionToClient connection, ClickerState state)
+        {
+            OnLocalStateChanged?.Invoke(state);
+        }
+
+        [TargetRpc]
+        private void TargetError(NetworkConnectionToClient connection, string code)
+        {
+            OnLocalError?.Invoke(code);
+        }
+
+        private ClickerState MakeState()
+        {
+            return new ClickerState(_balance, _multiplier, _upgradeLevel);
+        }
+
+        private int GetUpgradePrice(int level)
+        {
+            return 10 + level * 15;
         }
     }
 
